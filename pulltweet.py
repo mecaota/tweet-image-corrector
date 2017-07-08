6163d370ba06823f7bbac4300ec3c3a2d035d083
mecaota <mecaota@users.noreply.github.com>
2017-07-08 23:37:24 +0900

    画像あげているツイート・ユーザー名・tweetid・スクリーンネーム・原寸大画像URLだけを表示できるようにした

--- pulltweet.py
+++ pulltweet.py
@@ -10,11 +10,18 @@ with open('_settings.json','r') as f:
 
     twitter = OAuth1Session(api_keys["CONSUMER_KEY"], api_keys["CONSUMER_SECRET"], api_keys["ACCESS_TOKEN"], api_keys["ACCESS_TOKEN_SECRET"])
 
-    params = {"list_id":parameter["list_id"], "slug":parameter["user_id"]}
+    params = {"list_id":parameter["list_id"], "slug":parameter["user_id"], "include_entities":True, "count":40}
     req = twitter.get("https://api.twitter.com/1.1/lists/statuses.json", params = params
     )
 
     timeline = json.loads(req.text)
     for tweet in timeline:
-        print(tweet["text"])
-        print("--------------------")
+        if "extended_entities" in tweet and "media" in tweet["extended_entities"]:
+            #print(tweet)
+            print(tweet["user"]["screen_name"] + " " + tweet["user"]["name"])
+            print("TweetID:" + str(tweet["id"]))
+            print(tweet["text"])
+            for media in tweet["extended_entities"]["media"]:
+                pic = media["media_url"]+":orig"
+                print(pic)
+            print("-----------------------------")
