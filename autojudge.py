bfab8d6ffb7d76c58eaf320f4a718b0c7ebd439b
mecaota <mecaota@users.noreply.github.com>
2017-08-23 23:08:34 +0900

    pulltweetで別のAPIでツイート取得にも対応できるようsetting.jsonの記入形式とpulltweet.pyコードを変更

--- autojudge.py
+++ autojudge.py
@@ -9,11 +9,15 @@ if __name__ == "__main__":
     # Kerasに含まれるMNISTデータの取得
     # 初回はダウンロードが発生するため時間がかかる
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    
+
     # 配列の整形と、色の範囲を0-255 -> 0-1に変換
     X_train = X_train.reshape(60000, 784) / 255
+    print("X_train")
+    print(X_train)
+    print("X_test")
+    print(X_test)
     X_test = X_test.reshape(10000, 784) / 255
-    
+
     # 正解データを数値からダミー変数の形式に変換
     # これは例えば0, 1, 2の3値の分類の正解ラベル5件のデータが以下のような配列になってるとして
     #   [0, 1, 2, 1, 0]
@@ -25,32 +29,37 @@ if __name__ == "__main__":
     #    [1, 0, 0]]
     # 列方向が0, 1, 2、行方向が各データに対応し、元のデータで正解となる部分が1、それ以外が0となるように展開してる
     y_train = np_utils.to_categorical(y_train)
+    print("y_train")
+    print(y_train)
+    print("y_test")
+    print(y_test)
     y_test = np_utils.to_categorical(y_test)
-    
+
     # ネットワークの定義
     # 各層や活性関数に該当するレイヤを順に入れていく
     # 作成したあとにmodel.add()で追加することも可能
     model = Sequential([
-            Dense(512, input_shape=(784,)),
-            Activation('sigmoid'),
-            Dense(10),
-            Activation('softmax')
-        ])
+        Dense(512, input_shape=(784,)),
+        Activation('sigmoid'),
+        Dense(10),
+        Activation('softmax')
+    ])
     # 損失関数、 最適化アルゴリズムなどを設定しモデルのコンパイルを行う
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-    
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='sgd', metrics=['accuracy'])
+
     # 学習処理の実行
-    hist = model.fit(X_train, y_train, batch_size=200, verbose=1, epochs=20, validation_split=0.1)
-    
+    hist = model.fit(X_train, y_train, batch_size=200,
+                     verbose=1, epochs=20, validation_split=0.1)
+
     # 予測
     score = model.evaluate(X_test, y_test, verbose=1)
     print('test accuracy : ', score[1])
 
-
-    ### 学習結果のグラフ化
+    # 学習結果のグラフ化
     loss = hist.history['loss']
     val_loss = hist.history['val_loss']
-    
+
     # lossのグラフ
     plt.plot(range(20), loss, marker='.', label='loss')
     plt.plot(range(20), val_loss, marker='.', label='val_loss')
@@ -59,10 +68,10 @@ if __name__ == "__main__":
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.show()
-    
+
     acc = hist.history['acc']
     val_acc = hist.history['val_acc']
-    
+
     # accuracyのグラフ
     plt.plot(range(20), acc, marker='.', label='acc')
     plt.plot(range(20), val_acc, marker='.', label='val_acc')
--- pulltweet.py
+++ pulltweet.py
@@ -9,68 +9,109 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-#ローカルのjsonファイル展開、返り値は辞書型
+
+# ローカルのjsonファイル展開、返り値は辞書型
 def openJSON(file):
-    with open(file,'r') as f:
+    with open(file, 'r') as f:
         return json.load(f)
 
-#ツイッター認証・ツイッターAPIオブジェクト生成
+
+# ツイッター認証・ツイッターAPIオブジェクト生成
 def oauthTwitter(api_keys):
-    twitter = OAuth1Session(api_keys["CONSUMER_KEY"], api_keys["CONSUMER_SECRET"], api_keys["ACCESS_TOKEN"], api_keys["ACCESS_TOKEN_SECRET"])
+    twitter = OAuth1Session(api_keys["CONSUMER_KEY"], api_keys["CONSUMER_SECRET"],
+                            api_keys["ACCESS_TOKEN"], api_keys["ACCESS_TOKEN_SECRET"])
     return twitter
 
-#指定したツイートを各種取得
+
+# 指定したツイートを各種取得
 def getTweet(twitter, api, params):
     apiurl = "https://api.twitter.com/1.1/" + api + ".json"
-    req = twitter.get(apiurl, params = params)
+    req = twitter.get(apiurl, params=params)
     return json.loads(req.text)
 
-#twitter画像読み込み
-def showTweetimg(tweet):
+
+# twitter画像読み込み
+def showTweetimg(tweet, selecter=0):
+    showTweet(tweet)
+    for media in tweet["extended_entities"]["media"]:
+        pic = media["media_url"] + ":orig"
+        print(pic)
+        downloadImage(pic, selecter)
+    print("-----------------------------")
+
+
+# tweet表示
+def showTweet(tweet):
     print(tweet["user"]["screen_name"] + " " + tweet["user"]["name"])
     print("TweetID:" + str(tweet["id"]))
     print(tweet["text"])
-    for media in tweet["extended_entities"]["media"]:
-        pic = media["media_url"]+":orig"
-        print(pic)
-        imgDownload(pic)
-    print("-----------------------------")
 
-#url経由画像ダウンロード
-def imgDownload(url):
-    img_read = urllib.request.urlopen(url).read() #URLよりバイナリストリームで画像取得
-    img_arr = np.fromstring(img_read, np.uint8) #ストリームをnumpy arrayに変換
+
+# 実行必要性可否判定
+def judge():
+    result = str(input("これ要る？"))
+    if result is ("y" or ""):
+        print(result)
+        return True
+    else:
+        return False
+
+
+# url経由画像ダウンロード
+def downloadImage(url, selecter=0):
+    img_read = urllib.request.urlopen(url).read()  # URLよりバイナリストリームで画像取得
+    img_arr = np.fromstring(img_read, np.uint8)  # ストリームをnumpy arrayに変換
     image = cv2.imdecode(img_arr, -1)
     if image is None:
-        raise ValueError("'" + url +"'" + "はこのプログラムでは扱えないファイルです。このファイルはパスされます。")
+        raise ValueError("'" + url + "'" +
+                         "はこのプログラムでは扱えないファイルです。このファイルはパスされます。")
         return None
     #reg = r"[\/\.{a-z}+\:orig$]"
-    filename = re.sub(r"[\/\.{a-z}+\:orig$]","",urlparse(url).path) #urlを解析してpath部分を抽出、正規表現でファイル名以外を削除
-    path = "sample/" + filename + ".png"
+    # urlを解析してpath部分を抽出、正規表現でファイル名以外を削除
+    filename = re.sub(r"[\/\.{a-z}+\:orig$]", "", urlparse(url).path)
+    path = "sample/" + str(selecter) + "/" + filename + ".png"
     print(path)
-
-    #画像処理
-    optimage = imageOptimize(image)
-    cv2.imwrite(path, optimage)
-    #画像プレビュー
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #plt.imshow(image)
-    #plt.show()
+    saveImage(image, path)
     return 0
 
-def imageOptimize(image):
+
+# 画像保存
+def saveImage(image, path):
+    # 画像処理
+    optimage = optimizeImage(image)
+    cv2.imwrite(path, optimage)
+    # 画像プレビュー
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # plt.imshow(image)
+    # plt.show()
+
+
+# 画像処理
+def optimizeImage(image):
     #image = cv2.cvtColor(image)
     cv2.resize(image, (500, 500))
     return image
 
+
 if __name__ == "__main__":
-    settings = openJSON("_settings.json") #setting.JSON open
-    api_keys = settings["API_KEY"] #APIkey
-    parameter = settings["PARAMETER"] #twitterAPIパラメーター
-    twitter = oauthTwitter(api_keys) #oauth認証にてツイッターオブジェクト生成
-    params = {"list_id":parameter["list_id"], "slug":parameter["user_id"], "include_entities":True, "count":200}
-    timeline = getTweet(twitter, "lists/statuses", params)
-    for tweet in timeline:
-        if "extended_entities" in tweet and "media" in tweet["extended_entities"]:
-            showTweetimg(tweet)
-            #break #画像1枚だけ拾いたいときコメントアウト
+    settings = openJSON("_settings.json")  # setting.JSON open
+    api_keys = settings["API_KEY"]  # APIkey
+    selecter = int(input("Please select PARAMETER array:"))
+    parameter = settings["PARAMETER"][selecter]  # twitterAPIパラメーター
+    type = parameter["api_type"]
+    twitter = oauthTwitter(api_keys)  # oauth認証にてツイッターオブジェクト生成
+
+    # APIの種類に応じてパラメーターを変更
+    if "lists/statuses" in type:
+        params = {"list_id": parameter["list_id"],
+                  "slug": parameter["user_id"], "include_entities": True, "count": 200}
+    timeline = getTweet(twitter, type, params)  # タイムライン取得
+
+    # 画像取得処理
+    if timeline:
+        for tweet in timeline:
+            if "extended_entities" in tweet and "media" in tweet["extended_entities"]:
+                showTweetimg(tweet, selecter)
+                # break #画像1枚だけ拾いたいときコメントアウト
+    else:
+        print("まだ非対応のAPIを叩いたか、そもそもそんなAPIないです")
